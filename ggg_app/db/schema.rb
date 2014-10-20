6d2457ee9d7eeb886e76001011490f3df62b445d
luke-ohara <luke.c.ohara@gmail.com>
2014-10-20 09:47:00 +0100

    migrating to create teams table in db

--- ggg_app/db/schema.rb
+++ ggg_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141020083953) do
+ActiveRecord::Schema.define(:version => 20141020084606) do
 
   create_table "matches", :force => true do |t|
     t.string   "div"
@@ -82,6 +82,12 @@ ActiveRecord::Schema.define(:version => 20141020083953) do
     t.string   "season"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "teams", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
